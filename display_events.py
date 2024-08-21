02afe995c3aca7b86759b926f206b6f132b69574
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2024-08-21 14:51:38 +0200

    display_events simplified

--- display_events.py
+++ display_events.py
@@ -13,19 +13,18 @@ from tools import (
     recal_params,
     params,
     prepare_event,
+    tqdm,
 )
 
 
-def display_events(events, charge_df, light_df, match_dict, metrics=None):
-    for event in events:
+def display_events(events, charge_df, light_df=None, match_dict=None, metrics=None):
+    for event in tqdm(events):
         charge_event, light_event, _ = prepare_event(
-            charge_df, light_df, match_dict, event
+            event, charge_df, light_df, match_dict
         )
 
         if charge_event is None and light_event is None:
             continue
-
-        recal_params()
 
         event_display(
             event_idx=event,
@@ -36,12 +35,14 @@ def display_events(events, charge_df, light_df, match_dict, metrics=None):
 
         if params.show_figures:
             plt.show()
+        else:
+            plt.close("all")
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("file", help="Folder name for specific data file")
-    parser.add_argument("events", help="Event number", type=int, nargs="+")
+    parser.add_argument("-e", "--events", help="Event number", type=int, nargs="+")
     parser.add_argument("--save", "-s", help="Save images", action="store_true")
     parser.add_argument(
         "--no-display", "-n", help="Don't display images", action="store_false"
@@ -52,30 +53,42 @@ if __name__ == "__main__":
     params.show_figures = args.no_display
     params.file_label = args.file
     params.save_figures = args.save
+
+    if args.events:
+        params.individual_plots = args.events
+
     recal_params()
 
     # Load charge file
-    charge_df = pd.read_csv(
-        f"{params.file_label}/charge_df_{params.file_label}.bz2", index_col="eventID"
-    )
-    charge_df[charge_df.columns[9:]] = charge_df[charge_df.columns[9:]].map(
-        lambda x: literal_eval(x) if isinstance(x, str) else x
+    charge_input = f"{params.file_label}/charge_df_{params.file_label}.bz2"
+    charge_df = pd.read_csv(charge_input, index_col="eventID")
+    charge_df[charge_df.columns] = charge_df[charge_df.columns].map(
+        lambda x: (
+            literal_eval(x)
+            if isinstance(x, str) and (x[0] == "[" or x[0] == "(")
+            else x
+        )
     )
 
     # Load light file
-    light_df = pd.read_csv(f"{params.file_label}/light_df_{params.file_label}.bz2")
+    light_input = f"{params.file_label}/light_df_{params.file_label}.bz2"
+    light_df = None
+    match_dict = None
+    if os.path.isfile(light_input):
+        light_df = pd.read_csv()
 
-    # Load match dictionary
-    match_dict = json.load(
-        open(f"{params.file_label}/match_dict_{params.file_label}.json")
-    )
-    match_dict = {int(key): value for key, value in match_dict.items()}
+        # Load match dictionary
+        match_dict = json.load(
+            open(f"{params.file_label}/match_dict_{params.file_label}.json")
+        )
+        match_dict = {int(key): value for key, value in match_dict.items()}
 
     # Load metrics file
-    if os.path.isfile(f"{params.file_label}/metrics_{params.file_label}.pkl"):
-        with open(f"{params.file_label}/metrics_{params.file_label}.pkl", "rb") as f:
+    input_metrics = f"{params.file_label}/metrics_{params.file_label}.pkl"
+    if os.path.isfile(input_metrics):
+        with open(input_metrics, "rb") as f:
             metrics = pickle.load(f)
     else:
         metrics = None
 
-    display_events(args.events, charge_df, light_df, match_dict, metrics)
+    display_events(params.individual_plots, charge_df, light_df, match_dict, metrics)
