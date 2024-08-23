b763c26c285e6704d76ba8d346f016f5f2c25662
Guilherme Ruiz <57478888+guigoruiz1@users.noreply.github.com>
2024-08-23 15:57:34 +0200

    MC processed files

--- batch_example.sh
+++ batch_example.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Loop through all files matching the pattern in the specified directory
-for file in ../Monte\ Carlo/singlecube_cry_hit_*.root
+for file in $SOLAREOS/montecarlo/singlecube/cosmic_v0/singlecube_cry_hit_*.root
 do
 
 echo "Processing" $file
 # Extract the numbers after 'hit_' and before '.root' using sed to get the output folder
-label=$(echo $file | sed -n 's/.*hit_\([0-9]*\)\.root/\1/p')_new
+label=$(echo $file | sed -n 's/.*hit_\([0-9]*\)\.root/\1/p')
 
 # Run the reconstruction script simulating dead areas and dh set to 30
 ./reconstruction.py "$file" -d -p file_label=$label
